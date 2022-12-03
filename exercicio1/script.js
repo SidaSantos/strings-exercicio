32ff8a4ef7353be19be212a8b7e24cced2cf0dfe
USUÁRIO <sidasantos6395@gmail.com>
2022-12-03 20:28:47 -0300

    resposta-exerxicios-de-string

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual o seu nome?")
+let comidaPreferida1 = prompt("qual a sua  primeira comida preferida?")
+let comidaPreferida2 = prompt("qual a sua segunda comida preferida?")
+let comidaPreferida3 = prompt("qual a sua terceira comida preferida?")
+
+console.log(`Estas são as comidas favoritas de ${nome}\n- ${comidaPreferida1}\n- ${comidaPreferida2}\n- ${comidaPreferida3}`)
